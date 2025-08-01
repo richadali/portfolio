2b73b8020a7c6a268de0cb3129fc29dcd38d9f18
richadali <richadyaminali@gmail.com>
2025-08-01 14:46:47 +0530

    Fixed mysql12 warnings

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,9 +9,7 @@ const dbConfig = {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  acquireTimeout: 60000,
-  timeout: 60000,
-  reconnect: true,
+  connectTimeout: 60000,
   charset: "utf8mb4",
 };
 
